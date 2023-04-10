b8df1acf414e1cedfe6610c2bf6f2172158bf001
Alexnder Chigryay <Fademon2019@gmail.com>
2023-04-10 23:26:27 +0700

    Done

--- Encapsulation/01_Elevator/src/ru/skillbox/Main.java
+++ Encapsulation/01_Elevator/src/ru/skillbox/Main.java
@@ -1,7 +1,24 @@
 package ru.skillbox;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-    public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Elevator elevator = new Elevator(1, 9);
+        elevator.printCurrentFloor();
+        elevator.moveUp();
+        elevator.printCurrentFloor();
+        elevator.moveDown();
+        elevator.moveDown();
+        elevator.printCurrentFloor();
+        int floor = Integer.parseInt(reader.readLine());
+        elevator.move(floor);
+        elevator.printCurrentFloor();
+        floor = Integer.parseInt(reader.readLine());
+        elevator.move(floor);
+        elevator.printCurrentFloor();
     }
 }
