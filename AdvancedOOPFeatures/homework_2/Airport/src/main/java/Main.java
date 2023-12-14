1443078cf151f5dce6180e0e896e0d308976159f
Alex_Chiga <Fademon2019@gmail.com>
2023-12-15 00:11:34 +0700

    Added homework_15_2

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,29 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+
+import java.util.Date;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+        Airport a1 = Airport.getInstance();
 
+        findPlanesLeavingInTheNextTwoHours(a1);
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+
+        Date currentTime = new Date();
+        return airport.getTerminals()
+                .stream()
+                .flatMap(t -> t.getFlights()
+                        .stream())
+                .filter(flight -> flight.getType().equals(Flight.Type.DEPARTURE)
+                        && currentTime.before(flight.getDate())
+                        && flight.getDate().getTime() - currentTime.getTime() <= 7_200_000)
+                .peek(System.out::println)
+                .toList();
     }
 
 }
