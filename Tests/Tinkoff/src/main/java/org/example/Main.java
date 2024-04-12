a974a70f3d516517fe596cef41a654c57e3e2ff6
Alex_Chiga <Fademon2019@gmail.com>
2024-04-12 19:06:49 +0700

    Del outher files

--- Tests/Tinkoff/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        String text = "code999andcodec1andcocode888";
-        text = text.replaceAll("code\\d+", "???");
-        System.out.println(text);
-    }
-}
