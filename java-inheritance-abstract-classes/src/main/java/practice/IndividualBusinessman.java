f61479ce02bf7d64aed2d0d9efe1a6e92ec47bb6
Alex Chiga <fademon2019@gmail.com>
2024-09-13 14:05:26 +0700

    del files

--- java-inheritance-abstract-classes/src/main/java/practice/IndividualBusinessman.java
+++
@@ -1,9 +0,0 @@
-package practice;
-
-public class IndividualBusinessman extends Client {
-    @Override
-    public void put(double amountToPut) {
-        super.put(amountToPut);
-        amount -= amountToPut < 1000 ? amountToPut * 0.01 : amountToPut * 0.005;
-    }
-}
--- java-inheritance-abstract-classes/src/main/java/practice/PhysicalPerson.java
+++
@@ -1,8 +0,0 @@
-package practice;
-
-public class PhysicalPerson extends Client {
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-}
