4afa988487b9e1b933db9c2e8cd225e43865da5d
Alex Chiga <fademon2019@gmail.com>
2024-09-28 17:32:13 +0700

    Homework 10 - 1 added

---
+++ projects_homeworks/Homework-10/java_collectionsarray/src/main/java/practice/reverseArray/ReverseArray.java
@@ -0,0 +1,19 @@
+package practice.reverseArray;
+
+import java.util.*;
+import java.util.Collection.*;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        String[] arr = {"a", "b", "c", "d"};
+        System.out.println(Arrays.toString(reverse(arr)));
+    }
+    public static String[] reverse(String[] strings) {
+        List<String> list = Arrays.asList(strings);
+        Collections.reverse(list);
+        strings = list.toArray(strings);
+        return strings;
+    }
+
+}
