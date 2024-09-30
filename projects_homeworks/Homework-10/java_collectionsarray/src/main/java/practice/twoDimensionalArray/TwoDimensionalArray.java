282c7ad46902abf716df892be68258203a091f4b
Alex Chiga <fademon2019@gmail.com>
2024-09-30 12:56:25 +0700

    Homework 10-3 added

--- projects_homeworks/Homework-10/java_collectionsarray/src/main/java/practice/twoDimensionalArray/TwoDimensionalArray.java
+++ projects_homeworks/Homework-10/java_collectionsarray/src/main/java/practice/twoDimensionalArray/TwoDimensionalArray.java
@@ -1,8 +1,18 @@
 package practice.twoDimensionalArray;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class TwoDimensionalArray {
 
     public static final char SYMBOL = 'X';
+
+    public static void main(String[] args) {
+        char[][] arr = getTwoDimensionalArray(7);
+        for (char[] chars : arr) {
+            System.out.println(Arrays.toString(chars));
+        }
+    }
 
     public static char[][] getTwoDimensionalArray(int size) {
 
@@ -12,7 +22,25 @@ public class TwoDimensionalArray {
         // [ , X,  ]
         // [X,  , X]
 
+        char[][] arr = new char[size][size];
+        int pos1 = 0;
+        int pos2 = arr.length - 1;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                arr[i][j] = ' ';
+                arr[i][pos1] = 'X';
+                arr[i][pos2] = 'X';
+            }
+            if (pos2 - pos1 != 0) {
+                pos1++;
+                pos2--;
+            } else {
+                pos1++;
+                pos2--;
+            }
 
-        return new char[0][0];
+        }
+
+        return arr;
     }
 }
