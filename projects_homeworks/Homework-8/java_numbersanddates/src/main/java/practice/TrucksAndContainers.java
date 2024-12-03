d386c2dca672b2213f306e6e50fd9b2020d84067
Alex Chiga <fademon2019@gmail.com>
2024-12-04 01:05:41 +0700

    Homework 8 - 1 added

--- projects_homeworks/Homework-8/java_numbersanddates/src/main/java/practice/TrucksAndContainers.java
+++ projects_homeworks/Homework-8/java_numbersanddates/src/main/java/practice/TrucksAndContainers.java
@@ -3,46 +3,49 @@ package practice;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class TrucksAndContainers {
 
+    private static int finalCountCar = 1;
+    private static int finalCountContainers = 1;
+
+    private final static int COUNT_BOXES_IN_CONTAINERS = 27;
+    private final static int COUNT_CONTAINERS_IN_CAR = 12;
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        //получение количество коробок от пользователя
+//        System.out.println("Сколько ящиков?");
+
         int countBoxes = Integer.parseInt(reader.readLine());
-        int countTrucks = 1;
-        int countContainers = 1;
-        final int MAX_COUNT_CONTAINERS_IN_THE_TUCK = 12;
-        final int MAX_COUNT_BOXES_IN_THE_CONTAINER = 27;
 
-        System.out.println("Грузовик: " + countTrucks);
+        for (int i = 1; i <= countBoxes;) {
 
-        for (int i = 1; i <= countBoxes; ) {
+            finalCountCar = getCountCar();
 
-            if (countContainers % (MAX_COUNT_CONTAINERS_IN_THE_TUCK + 1) == 0)
-                System.out.println("Грузовик: " + countTrucks);
+            if (finalCountContainers % COUNT_CONTAINERS_IN_CAR - 1 == 0)
+                System.out.println("Грузовик: " + finalCountCar);
+            System.out.println("\tКонтейнер: " + finalCountContainers);
 
-            System.out.println("\tКонтейнер: " + countContainers);
-            for (int j = 1; j <= MAX_COUNT_BOXES_IN_THE_CONTAINER && i <= countBoxes; j++) {
-                countContainers = i % 27 == 0 ?
-                        countContainers + 1 :
-                        countContainers;
+            for (int j = 1; j <= COUNT_BOXES_IN_CONTAINERS && i <= countBoxes; j++) {
 
                 System.out.println("\t\tЯщик: " + i);
                 i++;
             }
 
-            countTrucks = countContainers % MAX_COUNT_CONTAINERS_IN_THE_TUCK == 0 ?
-                    countTrucks + 1 :
-                    countTrucks;
+            finalCountContainers = i > countBoxes ?
+                    finalCountContainers :
+                    finalCountContainers + 1;
         }
+        System.out.printf("Необходимо:\nгрузовиков - %d шт.\nконтейнеров - %d шт.",
+                finalCountCar, finalCountContainers);
+    }
 
-        System.out.println("Необходимо:\nгрузовиков - " + countTrucks +
-                " шт.\nконтейнеров - " +
-                countContainers + " шт.");
-
+    private static int getCountCar() {
+        return finalCountContainers > COUNT_CONTAINERS_IN_CAR ?
+                finalCountCar + 1 :
+                finalCountCar;
     }
 
 }
