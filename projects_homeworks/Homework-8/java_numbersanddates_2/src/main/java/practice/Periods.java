8b7d3ed26a6d2723fe7830a3cdd315d28b75b29e
Alex Chiga <fademon2019@gmail.com>
2024-12-05 22:24:41 +0700

    homework 8 - 4 added

--- projects_homeworks/Homework-8/java_numbersanddates_2/src/main/java/practice/Periods.java
+++ projects_homeworks/Homework-8/java_numbersanddates_2/src/main/java/practice/Periods.java
@@ -2,14 +2,22 @@ package practice;
 
 import java.time.LocalDate;
 
+import java.time.temporal.ChronoUnit;
+
 public class Periods {
 
     // реализуйте вывод разницы между датами, используя класс Period
     public static String getPeriodFromBirthday(LocalDate firstDate, LocalDate secondDate) {
 
-        return  "years:" + Math.abs(secondDate.getYear() - firstDate.getYear()) +
-                ", months:" + Math.abs(secondDate.getMonthValue() - firstDate.getMonthValue()) +
-                ", days:" + Math.abs(secondDate.getDayOfMonth() - firstDate.getDayOfMonth());
+        String years =  "years:" + ChronoUnit.YEARS.between(firstDate,
+                secondDate);
+        secondDate = secondDate.minusYears(secondDate.getYear() - firstDate.getYear());
+        String month =  "months:" + ChronoUnit.MONTHS.between(firstDate, secondDate);
+        String days = "days:" + ChronoUnit.DAYS.between(firstDate, secondDate);
+
+        return years + ", " + month + ", " + days;
     }
+
+
 
 }
