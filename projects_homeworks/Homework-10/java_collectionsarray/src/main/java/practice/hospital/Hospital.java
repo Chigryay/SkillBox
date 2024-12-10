1bd5982bd2679aef32cdc92efecbab22ccecb34d
Alex Chiga <fademon2019@gmail.com>
2024-12-11 01:52:45 +0700

    Homework 10-3 на доработку

--- projects_homeworks/Homework-10/java_collectionsarray/src/main/java/practice/hospital/Hospital.java
+++ projects_homeworks/Homework-10/java_collectionsarray/src/main/java/practice/hospital/Hospital.java
@@ -6,16 +6,7 @@ import java.util.Random;
 import java.util.stream.Stream;
 
 public class Hospital {
-    private final static float maxTemp = 42.1f;
-    private final static float minTemp = 31.9f;
-    private static final StringBuilder stringBuilder = new StringBuilder();
-    public static float[] generatePatientsTemperatures(int patientsCount) {
-        float[] arr = new float[patientsCount];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) Math.round(new Random().nextFloat(minTemp, maxTemp) * 10) / 10;
-        }
-        return arr;
-    }
+
 
     public static String getReport(float[] temperatureData) {
         /*
@@ -25,22 +16,7 @@ public class Hospital {
             а температуры каждого пациента до 1 знака после запятой
         */
 
-        float averageTemp = 0;
-        int count = 0;
-        for (int i = 0; i < temperatureData.length; i++) {
-            averageTemp += temperatureData[i];
-            stringBuilder.append(temperatureData[i]).append(" ");
-            if (temperatureData[i] >= 36.2 && temperatureData[i] <= 36.9) {
-                count++;
-            }
-        }
 
-        averageTemp = (float) Math.round((averageTemp / temperatureData.length) * 100) / 100;
-        String report =
-            "Температуры пациентов: " + stringBuilder.toString().trim() +
-            "\nСредняя температура: " + averageTemp +
-            "\nКоличество здоровых: " + count;
-
-        return report;
+        return "";
     }
 }
--- projects_homeworks/Homework-10/java_collectionsarray/src/main/java/practice/hospital/Main.java
+++ projects_homeworks/Homework-10/java_collectionsarray/src/main/java/practice/hospital/Main.java
@@ -8,8 +8,6 @@ public class Main {
         // используя данный метод, вы можете использовать ваши методы, и проверить их работу на разных
         // значений для проверки и отладки
 
-        float[] temperatureData = Hospital.generatePatientsTemperatures(30);
-        System.out.println(Hospital.getReport(temperatureData));
 
         //Пример вывода в консоль:
         //Температуры пациентов: 36.7 38.9 34.7
